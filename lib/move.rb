a81e8290d7d270db3c201c11a1ffa02d7c5b2ffa
Jeamay Palo <jeamay.palo@gmail.com>
2018-01-14 22:26:03 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,7 +5,6 @@ def input_to_index(user_input)
   puts "#{user_input}".to_i + 1
 end
 board = [" ", " ", " ", " "," ", " ", " ", " ", " "]
-index = gets
 def move (board, index, player = "X")
   board[index] = player
 end
