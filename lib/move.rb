d9ccb7b1af3a86b575ea50c888b451e51340d0cf
Jeamay Palo <jeamay.palo@gmail.com>
2018-01-14 21:59:06 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,14 @@
+
+
+user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+def input_to_index(user_input)
+  puts "#{user_input}".to_i + 1
+end
+board = [" ", " ", " ", " "," ", " ", " ", " ", " "]
+character = ["X", "O"]
+index = gets.strip
+def move (board, index, character)
+end
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +16,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 
 # code your input_to_index and move method here!
